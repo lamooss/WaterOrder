12b0c2437bcb1264a76edc1cd75346dd835d621e
lamooss <lamooss@naver.com>
2017-12-26 08:32:49 +0900

    2017-12-19 : SMS Basic

--- app/src/main/java/com/techdmz/lamooss/waterorder/WaterListFragment.java
+++ app/src/main/java/com/techdmz/lamooss/waterorder/WaterListFragment.java
@@ -1,8 +1,11 @@
 package com.techdmz.lamooss.waterorder;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -10,6 +13,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.Space;
 import android.support.v7.app.AlertDialog;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +23,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -155,12 +160,18 @@ public class WaterListFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // 1. SMS발송 용 코드
+                sendSMS(getView());
+
+                // 2. 데이터전송 용 코드
+                /*
                 String data = TextViewSendData.getText().toString().trim();
                 send(data);
 
                 Intent intent = new Intent((Activity)getContext(), MainActivity.class);
                 getContext().startActivity(intent);
-
+                */
             }
         });
 
@@ -292,6 +303,90 @@ public class WaterListFragment extends Fragment {
     public void println(String data)
     {
         logOutput.append(data + "\n");
+    }
+
+
+    // SMS
+    public void sendSMS(View v){
+        //String smsNum = smsNumber.getText().toString();
+        //String smsText = smsTextContext.getText().toString();
+
+        String smsNum = "01045789867";
+        String smsText = "SMS TESTING";
+
+        if (smsNum.length()>0 && smsText.length()>0)
+        {
+            sendSMS(smsNum, smsText);
+        }
+        else
+        {
+            Toast.makeText(WaterListFragment.this.getActivity(), "zzz", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void sendSMS(String smsNumber, String smsText){
+
+        PendingIntent sentIntent = PendingIntent.getBroadcast(WaterListFragment.this.getActivity(), 0, new Intent("SMS_SENT_ACTION"), 0);
+        PendingIntent deliveredIntent = PendingIntent.getBroadcast(WaterListFragment.this.getActivity(), 0, new Intent("SMS_DELIVERED_ACTION"), 0);
+
+        WaterListFragment.this.getActivity().registerReceiver(new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch(getResultCode()){
+
+                    case Activity.RESULT_OK:
+                        // 전송 성공
+                        Toast.makeText(WaterListFragment.this.getActivity(), "전송 완료", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        // 전송 실패
+                        Toast.makeText(WaterListFragment.this.getActivity(), "전송 실패", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        // 서비스 지역 아님
+                        Toast.makeText(WaterListFragment.this.getActivity(), "서비스 지역이 아닙니다", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        // 무선 꺼짐
+                        Toast.makeText(WaterListFragment.this.getActivity(), "무선(Radio)가 꺼져있습니다", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        // PDU 실패
+                        Toast.makeText(WaterListFragment.this.getActivity(), "PDU Null", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter("SMS_SENT_ACTION"));
+
+
+
+        WaterListFragment.this.getActivity().registerReceiver(new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()){
+
+                    case Activity.RESULT_OK:
+                        // 도착 완료
+                        Toast.makeText(WaterListFragment.this.getActivity(), "SMS 도착 완료", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case Activity.RESULT_CANCELED:
+                        // 도착 안됨
+                        Toast.makeText(WaterListFragment.this.getActivity(), "SMS 도착 실패", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter("SMS_DELIVERED_ACTION"));
+
+        SmsManager mSmsManager = SmsManager.getDefault();
+        mSmsManager.sendTextMessage(smsNumber, null, smsText, sentIntent, deliveredIntent);
     }
 
     @Override
